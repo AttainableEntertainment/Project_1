8efafa05222247f45ab27f14afc43464c1ff571e
jonreis90 <jonreis90@hotmail.com>
2021-05-01 23:00:20 -0400

    In progress, created gamemode, gamemode uasset changes for testing

---
+++ Source/Project__1/GameFramework/GameModeBase_2D.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeBase_2D.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT__1_API AGameModeBase_2D : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
