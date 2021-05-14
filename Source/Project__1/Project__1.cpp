b5a573461d0b0721570f194d7d991a687db60631
Ian-Val <valeta.iac@gmail.com>
2021-05-14 11:50:46 -0400

    delete everything

--- Source/Project__1/Project__1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Project__1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project__1, "Project__1" );
